77e93d5418fa2391ea74ffa0349ccdc93e9280e3
batu <ba921@nyu.edu>
2018-05-10 10:21:51 -0700

    I set it up so that the system emails me when the training ends.

--- duelingDQN.py
+++ duelingDQN.py
@@ -1,6 +1,7 @@
 import gym_bizhawk
 import os
-import save_code from save_code
+import time
+from support_utils import save_hyperparameters, send_email
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D, Reshape
@@ -19,7 +20,9 @@ ENV_NAME = 'BizHawk-v1'
 # env = gym.make(ENV_NAME)
 env = gym_bizhawk.BizHawk()
 nb_actions = env.action_space.n
+# BREADCRUMBS_START
 window_length = 1
+# BREADCRUMBS_END
 
 # Sequential Model
 print(env.observation_space.shape)
@@ -71,7 +74,8 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
 folder_count = len([f for f in os.listdir(TB_path)])
-tb_folder_path = f'{TB_path}DQN_{ENV_NAME}_run{folder_count + 1}'
+run_name = f"DQN_{ENV_NAME}_run{folder_count + 1}"
+tb_folder_path = f'{TB_path}{run_name}'
 
 os.mkdir(tb_folder_path)
 
@@ -79,10 +83,12 @@ with open(f"{tb_folder_path}/run_summary.txt", "w") as f:
     model.summary(print_fn=lambda x: f.write(x + '\n'))
 
 # This function saves all the important hypterparameters to the run summary file.
-save_code(["duelingDQN.py", "gym_bizhawk.py"], f"{tb_folder_path}/run_summary.txt")
+save_hyperparameters(["duelingDQN.py", "gym_bizhawk.py"], f"{tb_folder_path}/run_summary.txt")
 
+start_time_ascii = time.asctime(time.localtime(time.time()))
+start_time = time.time()
 # BREADCRUMBS_START
-dqn.fit(env, nb_steps=100000, visualize=True, verbose=0, callbacks=[callbacks.TensorBoard(log_dir=tb_folder_path, write_graph=False)])
+dqn.fit(env, nb_steps=1025, visualize=True, verbose=0, callbacks=[callbacks.TensorBoard(log_dir=tb_folder_path, write_graph=False)])
 # BREADCRUMBS_END
 
 file_count = len([f for f in os.listdir(models_path) if os.path.isfile(os.path.join(models_path, f))])
@@ -91,5 +97,8 @@ file_count = len([f for f in os.listdir(models_path) if os.path.isfile(os.path.j
 # After training is done, we save the final weights.
 dqn.save_weights('{}\DQN_{}_run{}_weights.h5f'.format(tb_folder_path, ENV_NAME, file_count + 1), overwrite=True)
 
+total_run_time = round(time.time() - start_time, 2)
+print("Training is done.")
+send_email(f"The training of {run_name} finalized!\nIt started at {start_time_ascii} and took {total_run_time/60} minutes .")
 # Finally, evaluate our algorithm for 5 episodes.
-dqn.test(env, nb_episodes=5, visualize=True)
+# dqn.test(env, nb_episodes=0, visualize=True)
